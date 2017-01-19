66cbbdd2c7d840479e7e674f8e187cc3e5d9c88c
thebadscarecrow <anais.pignet@hotmail.fr>
2017-01-19 19:10:07 +0100

    Add files via upload

--- src/commandes_externes.c
+++ src/commandes_externes.c
@@ -12,17 +12,24 @@ t_bool	ActionEXEC (parse_info *info, int debut, int nbArg) {
   char ligne[CHAINE_MAX];
   t_bool premierPlan;
   int i;
-
+  //On initialise ligne
   strcpy(ligne, "");
+  //Tant que i < nbArg, on incrémente i
   for(i=0; i<nbArg; i++) {
-
+    //
     if(strlen(ligne)!=0) {
       strcat(ligne," ");
     }
     strcat(ligne, info->ligne_cmd[debut+i]);
+
+    //Debug - faire un exec avec nos params pour la commande
+    printf("Ligne = %s\n", ligne);
+
   }
 
   premierPlan = (info->modificateur[debut]!=ARRIERE_PLAN);
+
+  
 
   printf("execution d'une commande externe  (%s %d) a ecrire :\n%s\n", __FILE__, __LINE__, ligne);
   (void) premierPlan;
