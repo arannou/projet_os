ae0def48656b680c9026b5ac8647eb68055c1fa0
thebadscarecrow <anais.pignet@hotmail.fr>
2017-02-08 22:48:57 +0100

    Add files via upload

---
+++ commandes_externes.h
@@ -0,0 +1,10 @@
+#ifndef _COMMANDES_EXTERNES_H_
+#define _COMMANDES_EXTERNES_H_
+#include "divers.h"
+#include "parse.h"
+#include <sys/types.h>
+#include <sys/wait.h>
+
+t_bool ActionEXEC (parse_info *info, int debut, int nbArg);
+
+#endif
