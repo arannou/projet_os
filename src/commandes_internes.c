4b4ea02cba0ca66a4f442f6d81d808a62c23168b
thebadscarecrow <anais.pignet@hotmail.fr>
2017-02-12 15:30:52 +0100

    Ajout de commentaire pour la redirection

--- src/commandes_internes.c
+++ src/commandes_internes.c
@@ -8,7 +8,7 @@
 #include "commandes_internes.h"
 
 t_bool	ActionECHO (parse_info *info, int debut, int nbArg) {
-
+  /*Variables */
   int i;
   FILE *sortie;
   /* si l'appel est correctement ecrit, on a :
@@ -17,29 +17,33 @@ t_bool	ActionECHO (parse_info *info, int debut, int nbArg) {
    *
    * Par de lecture, mais une ecriture redirigee possible
    */
+   // Si la redirection est demandée
   if (!EST_EGAL(info->sortie, ""))
   {
+    //On ouvre le fichier
     sortie = fopen(info->sortie, "w");
-    if (sortie == NULL)
+    if (sortie == NULL) //S'il y a eu une erreur lors de la lecture
     {
       /* Traitement du cas où le fichier n'est pas accessible en écriture */
       printf("Erreur, le fichier demandé n'est pas accessible en écriture. Vous allez etre redirigé sur la sortie standard\n");
-     sortie=stdout;
+     sortie=stdout; //On redirige l'utilisateur sur la sortie standard
     }
   }
+  //Si la redirection n'est pas demandée
   else {
-    sortie=stdout;
+    sortie=stdout; //On dirige l'utilisateur sur la sortie standard
   }
-  
+
   i = 1;
   while(i<nbArg)  {
     fprintf(sortie, "%s ", info->ligne_cmd[debut+i]);
     i++;
   }
   printf("\n");
-
+  // Si la redirection est demandée
   if (!EST_EGAL(info->sortie, ""))
   {
+    //On peut fermer notre fichier de sortie
     fclose(sortie);
   }
 
